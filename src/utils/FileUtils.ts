b38aad32f20d15018a85c69bbdaf7a636c989b06
Guilherme Lima <guibiel-10@hotmail.com>
2024-03-11 00:06:55 -0300

    remove console log

--- src/utils/FileUtils.ts
+++ src/utils/FileUtils.ts
@@ -6,8 +6,6 @@ export class FileUtils {
       return;
     }
 
-    console.log(name);
-
     const blob = new Blob([csv], { type: "text/csv;charset=utf-8" });
     const url = URL.createObjectURL(blob);
     const link = document.createElement("a");
